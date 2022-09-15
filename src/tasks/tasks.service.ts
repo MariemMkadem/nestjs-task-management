5aaa60213f1abe10ea5adcff13bf3af9c185e455
Mariem Mkadem <mariem.mkadem@outlook.com>
2022-09-15 12:12:44 +0200

    get tasks

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
+
+@Injectable()
+export class TasksService {
+    private tasks : Task[] = []
+    getAllTasks() : Task[] {
+        return this.tasks;
+    }
+}
